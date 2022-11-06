1ff8b2bbdd61092a7c332deeee27217c271d38f6
Wim van der Voort <wvoort.66@gmail.com>
2022-11-06 00:17:25 +0000

    Update bootstrap layout and caching eviction schedule

--- fixture-service/src/main/java/com/wvoort/wc2022/fixtureservice/CacheConfig.java
+++ fixture-service/src/main/java/com/wvoort/wc2022/fixtureservice/CacheConfig.java
@@ -5,18 +5,20 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import javax.management.timer.Timer;
 import java.time.Instant;
 import java.util.Arrays;
 
 @Slf4j
 @Configuration
 @EnableCaching
+@EnableScheduling
 public class CacheConfig {
 
     private static final String FIXTURES = "fixtures";
@@ -29,7 +31,7 @@ public class CacheConfig {
     }
 
     @CacheEvict(allEntries = true, value = {FIXTURES})
-    @Scheduled(fixedDelay = 15 * 60 * 1000 ,  initialDelay = 10000)
+    @Scheduled(fixedDelay = Timer.ONE_MINUTE * 15)
     public void reportCacheEvict() {
         log.info("Fixtures cache flushed at {}", Instant.now());
     }
